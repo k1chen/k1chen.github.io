2cfc5f96112258f2e794dc7c9c2c7f4efa71fd4d
k1chen <chenaun@bc.edu>
2023-06-07 15:32:44 -0400

    added barebones html and js page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var slides = document.getElementsByClassName('slide'),
+    slider = document.getElementById('slider'),
+    cursor = 0
+    slideWidth = 0
+    topHeight = 0
+    slideCount = slides.length;
