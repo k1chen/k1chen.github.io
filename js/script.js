b71811d59951a918b7ede1312e12174876e79d3c
k1chen <chenaun@bc.edu>
2023-10-29 17:26:37 -0400

    redesign for 2023

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var slides = document.getElementsByClassName('slide'),
-    slider = document.getElementById('slider'),
-    cursor = 0
-    slideWidth = 0
-    topHeight = 0
-    slideCount = slides.length;
